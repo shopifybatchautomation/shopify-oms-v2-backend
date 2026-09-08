3dec433705e12fb9f35e3ddc2cae4da8a5f7b4ef
shopifybatchautomation <shopifybatchautomation@gmail.com>
2026-09-08 12:39:41 +0530

    Update products.model.js

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -11,6 +11,10 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    images: {
+      type: [String],
+      required: true,
+    },
   },
   {
     timestamps: true,
