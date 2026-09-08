0576e89749431f65bb3a8fcbee7d4882f127cc96
shopifybatchautomation <shopifybatchautomation@gmail.com>
2026-09-08 11:34:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(
       'https://shopifyomsv3.netlify.app',
       'https://shopifyomsv4.netlify.app',
       'https://product-finder.netlify.app',
+      'https://p-finder.netlify.app'
     ],
   })
 );
